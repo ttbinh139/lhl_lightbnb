feba6335eefaccbccd99b56fc58a7006d4ac5ef0
ttbinh139 <ttbinh139@gmail.com>
2022-06-03 04:03:51 +0000

    LighthouseBNB Users

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -18,7 +18,7 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
+  /* let user;
   for (const userId in users) {
     user = users[userId];
     if (user.email.toLowerCase() === email.toLowerCase()) {
@@ -27,7 +27,24 @@ const getUserWithEmail = function(email) {
       user = null;
     }
   }
-  return Promise.resolve(user);
+  return Promise.resolve(user); */
+  let query = `SELECT * FROM users WHERE users.email = $1`;
+
+  return pool
+    .query(
+      query, [email])
+    .then((result) => {
+      if (result.rows.length === 1) {
+        return result.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+      //return new Promise.reject(err);
+      return null;
+    });
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -37,7 +54,23 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  let query = `SELECT * FROM users WHERE users.id = $1`;
+
+  return pool
+    .query(
+      query, [id])
+    .then((result) => {
+      if (result.rows.length === 1) {
+        return result.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+      //return new Promise.reject(err);
+      return null;
+    });
 }
 exports.getUserWithId = getUserWithId;
 
@@ -48,10 +81,22 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
+  /* const userId = Object.keys(users).length + 1;
   user.id = userId;
   users[userId] = user;
-  return Promise.resolve(user);
+  return Promise.resolve(user); */
+  let query = `INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3) RETURNING *;`;
+  return pool.query(query , [user.name, user.email, user.password]
+  )
+  .then((result) => {
+    //console.log(result.rows[0]);
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+    return null;
+  })
 }
 exports.addUser = addUser;
 
